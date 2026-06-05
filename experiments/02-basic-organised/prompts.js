290a04c1c37f2267f56e9b1b20c929a111c9356d
kt <207620413+tkpuk@users.noreply.github.com>
2026-06-05 13:17:56 +0100

    folder structure updated

---
+++ experiments/02-basic-organised/prompts.js
@@ -0,0 +1,12 @@
+export default function () {
+  return [
+    {
+      id: 'tweet-basic',
+      raw: 'Write a tweet about {{topic}}',
+    },
+    {
+      id: 'tweet-funny',
+      raw: 'Write a concise, funny tweet about {{topic}}',
+    },
+  ];
+}
